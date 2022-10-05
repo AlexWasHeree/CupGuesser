18c96d7d2301ea45b9ae65fa13b5b9b3254b5a3a
AlexWasHeree <victoralexandre2303@gmail.com>
2022-10-05 20:02:51 -0300

    input component

---
+++ CupGuesser/src/Components/input/index.jsx
@@ -0,0 +1,17 @@
+const index = ({ name, label, ...props }) => {
+  return (
+    <div className="flex flex-col">
+      <label htmlFor={name} className="text-sm font-bold text-grey-500 mb-2">
+        {label}
+      </label>
+      <input
+        className="p-3 border border-grey-700 rounded-xl"
+        {...props}
+        id={name}
+        name={name}
+      />
+    </div>
+  );
+};
+
+export default index;
--- CupGuesser/src/pages/Signup/index.jsx
+++ CupGuesser/src/pages/Signup/index.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import { Icon } from '../../Components';
-
-const Input = ({ name, label, ...props }) => {
-  return (
-    <div className="flex flex-col">
-      <label htmlFor={name} className="text-sm font-bold text-grey-500 mb-2">
-        {label}
-      </label>
-      <input
-        className="p-3 border border-grey-700 rounded-xl"
-        {...props}
-        id={name}
-        name={name}
-      />
-    </div>
-  );
-};
+import Input from '../../Components/input';
 
 export const Signup = () => {
   return (
